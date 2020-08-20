198214edef1ca2b2aaaac4c06353b561f91788d9
cpbeeker <gloominati.05@gmail.com>
2020-08-20 14:06:52 +0000

    last touches

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode( '/', $uri );
+$uri = explode('/', $uri);
 
 // all of our endpoints start with /person
 // everything else results in a 404 Not Found
@@ -24,8 +24,42 @@ if (isset($uri[2])) {
     $userId = (int) $uri[2];
 }
 
+// authenticate the request with Okta:
+if (! authenticate()) {
+    header("HTTP/1.1 401 Unauthorized");
+    exit('Unauthorized');
+}
+
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
-// pass the request method and user ID to the PersonController and process the HTTP request:
+// pass the request method and user ID to the PersonController:
 $controller = new PersonController($dbConnection, $requestMethod, $userId);
 $controller->processRequest();
+
+function authenticate() {
+    try {
+        switch(true) {
+            case array_key_exists('HTTP_AUTHORIZATION', $_SERVER) :
+                $authHeader = $_SERVER['HTTP_AUTHORIZATION'];
+                break;
+            case array_key_exists('Authorization', $_SERVER) :
+                $authHeader = $_SERVER['Authorization'];
+                break;
+            default :
+                $authHeader = null;
+                break;
+        }
+        preg_match('/Bearer\s(\S+)/', $authHeader, $matches);
+        if(!isset($matches[1])) {
+            throw new \Exception('No Bearer Token');
+        }
+        $jwtVerifier = (new \Okta\JwtVerifier\JwtVerifierBuilder())
+            ->setIssuer(getenv('OKTAISSUER'))
+            ->setAudience('api://default')
+            ->setClientId(getenv('OKTACLIENTID'))
+            ->build();
+        return $jwtVerifier->verify($matches[1]);
+    } catch (\Exception $e) {
+        return false;
+    }
+}
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,34 +7,213 @@ namespace Composer\Autoload;
 class ComposerStaticInitcd2ad2af202c23f4a9f553e70195f6d2
 {
     public static $files = array (
+        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
+        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        '9c67151ae59aff4788964ce8eb2a0f43' => __DIR__ . '/..' . '/clue/stream-filter/src/functions_include.php',
+        'a4ecaeafb8cfb009ad0e052c90355e98' => __DIR__ . '/..' . '/beberlei/assert/lib/Assert/functions.php',
+        '8cff32064859f4559445b89279f3199c' => __DIR__ . '/..' . '/php-http/message/src/filters.php',
+        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
+        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
     );
 
     public static $prefixLengthsPsr4 = array (
         'S' => 
         array (
+            'Symfony\\Polyfill\\Php80\\' => 23,
+            'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Ctype\\' => 23,
+            'Symfony\\Contracts\\Translation\\' => 30,
+            'Symfony\\Component\\Yaml\\' => 23,
+            'Symfony\\Component\\Translation\\' => 30,
+            'Symfony\\Component\\OptionsResolver\\' => 34,
             'Src\\' => 4,
+        ),
+        'P' => 
+        array (
+            'Psr\\Http\\Message\\' => 17,
+            'Psr\\Cache\\' => 10,
+        ),
+        'O' => 
+        array (
+            'Okta\\JwtVerifier\\' => 17,
+        ),
+        'M' => 
+        array (
+            'Mdanter\\Ecc\\' => 12,
+        ),
+        'J' => 
+        array (
+            'Jose\\' => 5,
+        ),
+        'H' => 
+        array (
+            'Http\\Promise\\' => 13,
+            'Http\\Message\\' => 13,
+            'Http\\Discovery\\' => 15,
+            'Http\\Client\\Curl\\' => 17,
+            'Http\\Client\\Common\\' => 19,
+            'Http\\Client\\' => 12,
+        ),
+        'G' => 
+        array (
+            'GuzzleHttp\\Psr7\\' => 16,
+        ),
+        'F' => 
+        array (
+            'FG\\' => 3,
         ),
         'D' => 
         array (
             'Dotenv\\' => 7,
         ),
+        'C' => 
+        array (
+            'Clue\\StreamFilter\\' => 18,
+        ),
+        'B' => 
+        array (
+            'Base64Url\\' => 10,
+        ),
+        'A' => 
+        array (
+            'Assert\\' => 7,
+            'AESKW\\' => 6,
+            'AESGCM\\' => 7,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
+        'Symfony\\Polyfill\\Php80\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-php80',
+        ),
+        'Symfony\\Polyfill\\Mbstring\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
+        ),
         'Symfony\\Polyfill\\Ctype\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
+        ),
+        'Symfony\\Contracts\\Translation\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/translation-contracts',
+        ),
+        'Symfony\\Component\\Yaml\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/yaml',
+        ),
+        'Symfony\\Component\\Translation\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/translation',
+        ),
+        'Symfony\\Component\\OptionsResolver\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/options-resolver',
         ),
         'Src\\' => 
         array (
             0 => __DIR__ . '/../..' . '/src',
         ),
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+        ),
+        'Psr\\Cache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/cache/src',
+        ),
+        'Okta\\JwtVerifier\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/okta/jwt-verifier/src',
+        ),
+        'Mdanter\\Ecc\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/mdanter/ecc/src',
+        ),
+        'Jose\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/spomky-labs/jose/src',
+        ),
+        'Http\\Promise\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/php-http/promise/src',
+        ),
+        'Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/php-http/message/src',
+            1 => __DIR__ . '/..' . '/php-http/message-factory/src',
+        ),
+        'Http\\Discovery\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/php-http/discovery/src',
+        ),
+        'Http\\Client\\Curl\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/php-http/curl-client/src',
+        ),
+        'Http\\Client\\Common\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/php-http/client-common/src',
+        ),
+        'Http\\Client\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/php-http/httplug/src',
+        ),
+        'GuzzleHttp\\Psr7\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
+        ),
+        'FG\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/fgrosse/phpasn1/lib',
+        ),
         'Dotenv\\' => 
         array (
             0 => __DIR__ . '/..' . '/vlucas/phpdotenv/src',
         ),
+        'Clue\\StreamFilter\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/clue/stream-filter/src',
+        ),
+        'Base64Url\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/spomky-labs/base64url/src',
+        ),
+        'Assert\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/beberlei/assert/lib/Assert',
+        ),
+        'AESKW\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/spomky-labs/aes-key-wrap/src',
+        ),
+        'AESGCM\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/spomky-labs/php-aes-gcm/src',
+        ),
+    );
+
+    public static $fallbackDirsPsr4 = array (
+        0 => __DIR__ . '/..' . '/nesbot/carbon/src',
+    );
+
+    public static $prefixesPsr0 = array (
+        'U' => 
+        array (
+            'UpdateHelper\\' => 
+            array (
+                0 => __DIR__ . '/..' . '/kylekatarnls/update-helper/src',
+            ),
+        ),
+    );
+
+    public static $classMap = array (
+        'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
+        'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
+        'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -42,6 +221,9 @@ class ComposerStaticInitcd2ad2af202c23f4a9f553e70195f6d2
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInitcd2ad2af202c23f4a9f553e70195f6d2::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInitcd2ad2af202c23f4a9f553e70195f6d2::$prefixDirsPsr4;
+            $loader->fallbackDirsPsr4 = ComposerStaticInitcd2ad2af202c23f4a9f553e70195f6d2::$fallbackDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInitcd2ad2af202c23f4a9f553e70195f6d2::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInitcd2ad2af202c23f4a9f553e70195f6d2::$classMap;
 
         }, null, ClassLoader::class);
     }
